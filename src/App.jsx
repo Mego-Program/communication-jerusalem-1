9b6c5e8d3ce34742da954053f042606937e391b1
Elazar Zlc <elazal1991@gmail.com>
2023-11-01 14:26:40 +0200

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+<<<<<<< HEAD
 
 
 function App() {
   
+=======
+import './App.css'
+import React from 'react';
+
+function App() {
+
+  return (
+    <>
+      <div >
+        <h1>Chats</h1>
+        </div>
+    </>
+  )
+>>>>>>> f186d0fc0888df8971f90a02fac5fbe0f0ac9dca
 }
 
 export default App
