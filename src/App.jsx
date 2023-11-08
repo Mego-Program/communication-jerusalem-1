bb189364d74110f7945a7626763b7b83b8d0bc62
Elazar Zlc <elazal1991@gmail.com>
2023-11-08 20:57:27 +0200

    update 8.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import MyTabs from './components/singeleOrGroup';
-
-
+import React from "react";
+import Grid from "@mui/material/Grid";
+import MyTabs from "./components/myTabs";
+import { Box } from "@mui/system";
+import ChatHistory from "./components/chatHistory";
 
 
 function App() {
-
   return (
-    <>
-    <MyTabs/>
-    </>
-  )
+    <Box sx={{background:'#21213E',
+
+    }}>
+      <Grid container spacing={1}>
+        <Grid item xs={2.5}>
+          <MyTabs />
+        </Grid>
+        <Grid item xs={9.5}></Grid>
+      </Grid>
+    </Box>
+  );
 }
 
-export default App
+export default App;
