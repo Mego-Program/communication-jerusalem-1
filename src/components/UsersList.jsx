5d8047b5d7c93a1e805dc5b15505313c9edeb06a
Sender Eisenbach <sender737@gmail.com>
2023-11-28 14:21:31 +0200

    add typing...

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import Reac, { useState, useEffect } from "react";
 import { ButtonGroup } from "@mui/material";
 import { Button } from "@mui/material";
 import { Box } from "@mui/system";
@@ -10,11 +10,39 @@ import Drawer from "./Drawer";
 export default function UsersList(props){
   const selected = props.selected;
   const items = props.items;
+  const [activity, setActivity] = useState("");
+  const [activityTimer, setActivityTimer] = useState(null);
+
+  useEffect(() => {
+    // Cleanup timer on component unmount
+    return () => clearTimeout(activityTimer);
+  }, [activityTimer]);
 
 
   const selectButton = (key) => {
     props.setSelected(key);
   };
+
+  
+  useEffect(() => {
+    // Listen for the "activity" socket event
+    props.socket.on("activity", (name) => {
+      setActivity(`${name} is typing...`);
+
+      // Clear after 3 seconds
+      clearTimeout(activityTimer);
+      setActivityTimer(
+        setTimeout(() => {
+          setActivity("");
+        }, 3000)
+      );
+    });
+
+    // Cleanup socket listener on component unmount
+    return () => {
+      props.socket.off("activity");
+    };
+  }, [activityTimer, props.socket]);
 
   return (
     <Box
@@ -49,7 +77,8 @@ export default function UsersList(props){
           >
             <Drawer />
             {objact.name}
-            
+            {activity && <span style={{ marginLeft: 8, fontSize: 12 }}>{activity}</span>}
+
           </Button>
         ))}
       </ButtonGroup>
