b3c212af4a9eb96faf740b23789a88eb23347675
Simcha Sucot <simchasucot@gmail.com>
2023-12-04 16:52:39 +0200

    Connecting VITE to INFRA

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppCommunication from './AppCommunication.jsx'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
-    <App />
+    <AppCommunication />
     </ThemeProvider>
 
 )
